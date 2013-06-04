21140b5d3843f5189d4516bb91c3368e44f30480
Philip Schiffer <philip.schiffer@sprylab.com>
2013-06-04 16:38:23 +0200

    fixed wrong timezone

--- server/php/includes/main.php
+++ server/php/includes/main.php
@@ -24,7 +24,7 @@
 ##  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ##  THE SOFTWARE.
 
-date_default_timezone_set('UTC');
+#date_default_timezone_set('UTC');
 
 require('plist.inc');
 require_once('config.inc');
